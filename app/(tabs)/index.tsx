6e910936f37d7f1b0d22de9138b9b7b7ede513e0
nasrelshamekh <nasrelshamekh@gmail.com>
2026-09-22 15:42:09 +0300

    implement fixes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -15,7 +15,7 @@ export default function App() {
             <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">Go to Sign In</Link>
             <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">Go to Sign Up</Link>
 
-            <Link href="/app/subscriptions/spotify">Spotify Subscription</Link>
+            <Link href="/subscriptions/spotify">Spotify Subscription</Link>
             <Link href={{pathname: "/subscriptions/[id]", params: {id: "Claude"},}}>Claude Max Subscription</Link>
 
         </SafeAreaView>
